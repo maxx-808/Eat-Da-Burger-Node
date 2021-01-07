83d8467aac8759d17c09e0d5dcf1252bb5133dc8
Maxx <maxhiga.hawaii@gmail.com>
2021-01-06 21:02:04 -1000

    ADDED: connection.js, connection to mysql server

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "Why9076ba11!",
+  database: "burgers_db",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error conecting: ${err.stack}`);
+    return;
+  }
+
+  console.log(`connected as id ${connection.threadId}`);
+});
