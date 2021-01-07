6b508d0d10cf6fb72e374c98bf2e2c889174e7d5
Maxx <maxhiga.hawaii@gmail.com>
2021-01-06 20:56:29 -1000

    wrote up schema and seeds and used mysql in commandline to start database and populate

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN NOT NULL default 0
+    devoured BOOLEAN NOT NULL DEFAULT(0),
+    
+    PRIMARY KEY (id)
 );
-
-
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Teriyaki Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Double Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Wagyu Burger");
